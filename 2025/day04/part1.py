09df29c46c7f344ca0d2fee9314d15764b6955c7
Hugo Simon <hugo.simon@etu.inp-n7.fr>
2025-12-04 20:08:49 +0100

    day 04

---
+++ 2025/day04/part1.py
@@ -0,0 +1,23 @@
+import fuckit
+
+with open("input") as f:
+    input = f.read().strip().splitlines()
+    input = [[int(i) for i in line.replace(".", "0").replace("@", "1")] for line in input]
+
+
+HEIGHT, WIDTH = len(input) + 1, len(input[0]) + 1
+
+
+@fuckit
+def voisins(lines, i, j):
+    res = 0
+    im = (i - 1) % HEIGHT
+    jm = min((j - 1) % WIDTH, j)
+    res += sum(lines[im][jm : j + 2])
+    res += sum(lines[i + 1][jm : j + 2])
+    res += lines[i][(j - 1) % WIDTH]
+    res += lines[i][j + 1]
+    return res
+
+
+print(sum((voisins(input, i, j) < 4) for i, line in enumerate(input) for j, value in enumerate(line) if value))
